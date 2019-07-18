825f2efb38c74affbac5bbf7f8ae11b4c99295b4
unknown <swathyphpfreelancer@gmail.com>
2019-07-18 17:10:51 -0400

    added imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
+//Angular Material modules
+import {CdkStepperModule} from '@angular/cdk/stepper';
+import { MatStepperModule,
+         MatFormFieldModule,
+         MatInputModule,
+         MatButtonModule,
+         MatSelectModule,
+         MatTableModule ,
+         MatSnackBarModule  
+      } 
+from '@angular/material';
+
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';  
 import { FullLayoutComponent } from './layout/full-layout/full-layout.component';
+import { ApiGeneratorComponent } from './api-generator/api-generator.component';
+import { ProjectGeneratorComponent } from './api-generator/project-generator/project-generator.component';
+import { DomainFieldGeneratorComponent } from './api-generator/domain-field-generator/domain-field-generator.component';
+import { ApiListGeneratorComponent } from './api-generator/api-list-generator/api-list-generator.component';
  
 
 @NgModule({
   declarations: [
     AppComponent,  
-    FullLayoutComponent
+    FullLayoutComponent,
+    ApiGeneratorComponent,
+    ProjectGeneratorComponent,
+    DomainFieldGeneratorComponent,
+    ApiListGeneratorComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +43,15 @@ import { FullLayoutComponent } from './layout/full-layout/full-layout.component'
     HttpClientModule,
     AppRoutingModule, 
     BrowserAnimationsModule,
-    FormsModule
+    FormsModule,
+    CdkStepperModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule ,
+    MatButtonModule,
+    MatTableModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
