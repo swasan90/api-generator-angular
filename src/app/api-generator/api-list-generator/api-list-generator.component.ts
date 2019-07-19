93e16f5638613ee93ac146c4ca871b65da042df6
unknown <swathyphpfreelancer@gmail.com>
2019-07-19 06:05:13 -0400

    created api list component to create master view detail for the material stepper

--- src/app/api-generator/api-list-generator/api-list-generator.component.ts
+++ src/app/api-generator/api-list-generator/api-list-generator.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ApiEndPoints } from 'src/app/model/api-endpoints';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-api-list-generator',
   templateUrl: './api-list-generator.component.html',
-  styleUrls: ['./api-list-generator.component.css']
+  styleUrls: ['../api-generator.component.css']
 })
 export class ApiListGeneratorComponent implements OnInit {
 
+  API_DATA:ApiEndPoints[]=[
+    {endpoint_name:"employees.delete",endpoint_url:"apigenerator/Geoscience/employees/ec9164b0-a7ed-11e9-bf1f-d51928148523",method_type:"DELETE"},
+    {endpoint_name:"employees.list",endpoint_url:"apigenerator/Geoscience/employees",method_type:"GET"}
+  ]
+  displayedColumns: string[] = ['endpoint_name', 'endpoint_url', 'method_type'];
+   
   constructor() { }
 
+  
+  
   ngOnInit() {
+    
   }
 
 }
