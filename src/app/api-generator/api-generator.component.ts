c8d20ebb510e2b8951777e8173c49ffd6adb730d
unknown <swathyphpfreelancer@gmail.com>
2019-07-19 06:08:07 -0400

    modified master view template for the material stepper

--- src/app/api-generator/api-generator.component.ts
+++ src/app/api-generator/api-generator.component.ts
@@ -16,41 +16,7 @@ export class ApiGeneratorComponent implements OnInit {
   domainFieldsFormGroup: FormGroup;
   isEditable = false;
 
-  fieldTypes: FieldType[] = [
-    { value: "number", viewValue: "Number" },
-    { value: "text", viewValue: "Text" },
-    { value: "boolean", viewValue: "Boolean" }
-  ];
-  
-  fieldControls: FormArray;
-
-  
-  API_DATA:ApiEndPoints[]=[
-    {endpoint_name:"employees.delete",endpoint_url:"apigenerator/Geoscience/employees/ec9164b0-a7ed-11e9-bf1f-d51928148523",method_type:"DELETE"},
-    {endpoint_name:"employees.list",endpoint_url:"apigenerator/Geoscience/employees",method_type:"GET"}
-  ]
-
-  displayedColumns: string[] = ['endpoint_name', 'endpoint_url', 'method_type'];
-
   constructor(private _formBuilder: FormBuilder) { }
-
-  submitProject() {
-
-  }
-
-  submitDomainFields() {
-    console.log("submitted fields");
-
-  }
-
-  add() {
-    console.log("clicked add");
-
-    this.fieldControls = this.domainFieldsFormGroup.get('fieldControls') as FormArray;
-    this.fieldControls.push(this.createFieldControls());
-  }
-
-
 
   createFieldControls(): FormGroup {
     return this._formBuilder.group({
@@ -59,18 +25,14 @@ export class ApiGeneratorComponent implements OnInit {
     });
   }
 
-
-
-
-  ngOnInit() {
+  ngOnInit() { 
     this.projectFormGroup = this._formBuilder.group({
       projectCtrl: ['', Validators.required],
       domainCtrl: ['', Validators.required]
     });
-
     this.domainFieldsFormGroup = this._formBuilder.group({
       fieldControls: this._formBuilder.array([this.createFieldControls()])
-    });
+    });   
 
   }
 
