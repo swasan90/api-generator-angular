f111aed5d4ee00bd1bac0dd96a5381b8d073db05
unknown <swathyphpfreelancer@gmail.com>
2019-07-19 06:03:31 -0400

    created project component to create master view detail for the material stepper

--- src/app/api-generator/project-generator/project-generator.component.ts
+++ src/app/api-generator/project-generator/project-generator.component.ts
@@ -1,26 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-project-generator',
   templateUrl: './project-generator.component.html',
-  styleUrls: ['./project-generator.component.css']
+  styleUrls: ['../api-generator.component.css']
 })
 export class ProjectGeneratorComponent implements OnInit {
   projectFormGroup: FormGroup;
   isEditable = false;
   constructor(private _formBuilder: FormBuilder) { }
 
+  @Input() projectForm:FormGroup;
   
   submitProject() {
 
   }
 
   ngOnInit() {
-    this.projectFormGroup = this._formBuilder.group({
-      projectCtrl: ['', Validators.required],
-      domainCtrl: ['', Validators.required]
-    });
+    // this.projectFormGroup = this._formBuilder.group({
+    //   projectCtrl: ['', Validators.required],
+    //   domainCtrl: ['', Validators.required]
+    // });
   }
 
 }
