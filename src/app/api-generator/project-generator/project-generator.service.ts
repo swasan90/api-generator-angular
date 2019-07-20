ead08bda026cb72ae433233714165576eb6a9174
unknown <swathyphpfreelancer@gmail.com>
2019-07-20 07:33:55 -0400

    created project generator service class to define service methods

--- src/app/api-generator/project-generator/project-generator.service.ts
+++ src/app/api-generator/project-generator/project-generator.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { ProjectDomain } from 'src/app/model/projectDomain';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectGeneratorService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+
+  public createProject(projectObj:ProjectDomain):Observable<any>{
+      return this.httpClient.post(environment.api_url+"createProject",projectObj);
+  }
 }
