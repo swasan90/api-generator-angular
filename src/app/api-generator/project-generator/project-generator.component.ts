deb6f1bd712ac0743d98cebf35878cded7989bc4
unknown <swathyphpfreelancer@gmail.com>
2019-07-23 20:22:09 -0400

    implemented snackbar service to show notification

--- src/app/api-generator/project-generator/project-generator.component.ts
+++ src/app/api-generator/project-generator/project-generator.component.ts
@@ -1,10 +1,12 @@
 import { ApiGeneratorService } from './../api-generator.service';
-import { Component, OnInit, Input, forwardRef } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl, ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Component, OnInit, forwardRef, ViewChild, AfterViewInit, } from '@angular/core';
+import {  FormGroup, Validators, FormControl, ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { ProjectDomain } from 'src/app/model/projectDomain';
 import { ProjectGeneratorService } from './project-generator.service';
-import {MatSnackBar} from '@angular/material';
 import { Project } from 'src/app/model/project';
+import { SnackbarService } from '../snackbar.service';
+import { MatStepper } from '@angular/material';
+ 
 
 @Component({
   selector: 'app-project-generator',
@@ -18,16 +20,20 @@ import { Project } from 'src/app/model/project';
     }
   ]
 })
-export class ProjectGeneratorComponent implements OnInit,ControlValueAccessor  {
+
+export class ProjectGeneratorComponent implements OnInit,ControlValueAccessor {
 
   public projectGeneratorForm :FormGroup  = new FormGroup({
       projectName: new FormControl("",[Validators.required]),
       domainName: new FormControl("",[Validators.required])
   });
+  isCompleted:boolean=false;
+   
+   
 
   isEditable = false;
   projData:Project;
-  constructor(private projectService:ProjectGeneratorService,private snackBar: MatSnackBar,private apiGeneratorService:ApiGeneratorService) { }
+  constructor(private projectService:ProjectGeneratorService,private snackBar: SnackbarService,private apiGeneratorService:ApiGeneratorService) { }
 
   public onTouched: () => void = () => {};
 
@@ -43,30 +49,30 @@ export class ProjectGeneratorComponent implements OnInit,ControlValueAccessor  {
   }
   setDisabledState?(isDisabled: boolean): void {
     isDisabled ? this.projectGeneratorForm.disable():this.projectGeneratorForm.enable();
-  }
-
-  openSnackBar(message: string, action: string,className:string) {     
-    this.snackBar.open(message, action, {
-      duration: 10000,
-      verticalPosition :'top',
-      horizontalPosition:'right',
-      panelClass:[className]
-    });
-  }
- 
+  } 
   
-  submitProject() {   
+  submitProject(stepper:MatStepper) {    
     let projObj:ProjectDomain = this.projectGeneratorForm.value;
     this.projectService.createProject(projObj).subscribe(data=>{      
       this.projData =  data["resObj"];
       localStorage.setItem("projectData",JSON.stringify(this.projData));      
-      this.openSnackBar(data["message"],"Success","custom-success-snackbar");
-      this.apiGeneratorService.getCurrentProject(this.projData);     
-    },error=>{
-      console.log(error);
-      this.openSnackBar(error.error["message"],"Error","custom-eror-snackbar");
+      this.snackBar.openSnackBar(data["message"],"Success","custom-success-snackbar");
+      this.apiGeneratorService.getCurrentProject(this.projData);   
+      this.isCompleted =true;    
+    },error=>{           
+      this.snackBar.openSnackBar(error.error["message"],"Error","custom-eror-snackbar");      
+      this.projectGeneratorForm.reset();
     })
+    
+  }
 
+  
+
+  formatString(fieldVal:string,event:any){        
+    this.projectGeneratorForm.get(event.target.attributes.getNamedItem('formcontrolname').value)
+    .setValue(
+        this.apiGeneratorService.cleanString(fieldVal)
+    );
   }
 
   ngOnInit() {       
