3c881b35466c0438cce3c0110d201fc4b67a5222
unknown <swathyphpfreelancer@gmail.com>
2019-07-24 13:07:07 -0400

    added view child to get stepper object and added completed property

--- src/app/api-generator/api-generator.component.ts
+++ src/app/api-generator/api-generator.component.ts
@@ -1,8 +1,11 @@
+import { MatStepper } from '@angular/material';
 import { Fields } from './../model/fields';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, QueryList } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormArray, FormControl } from '@angular/forms';
 import { FieldType } from '../model/fieldType';
 import { ApiEndPoints } from '../model/api-endpoints';
+import { ProjectGeneratorComponent } from './project-generator/project-generator.component';
+import { CdkStep } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-api-generator',
@@ -10,21 +13,25 @@ import { ApiEndPoints } from '../model/api-endpoints';
   styleUrls: ['./api-generator.component.css']
 })
 
- 
-export class ApiGeneratorComponent implements OnInit {
-  domainFieldGenerator:FormGroup;
-  projectDomainGenerator:FormGroup;
-  isEditable = false;
-  
-  constructor(private _fb:FormBuilder) { }
 
+export class ApiGeneratorComponent implements OnInit {
+  domainFieldGenerator: FormGroup;
+  projectDomainGenerator: FormGroup;
+  isEditable = false;
+  isCompleted :boolean = false;
+  @ViewChild('stepper') matStepper:MatStepper;  
+  constructor(private _fb: FormBuilder) { }
+
+   
+   
   ngOnInit() {
     this.projectDomainGenerator = this._fb.group({
-        projectInfo:new FormControl("") 
+      projectInfo: new FormControl("")     
     })
     this.domainFieldGenerator = this._fb.group({
-      domainFieldInfo:new FormControl("") 
-    });
+      domainFieldInfo: new FormControl("")
+    }); 
+   
   }
 
 }
