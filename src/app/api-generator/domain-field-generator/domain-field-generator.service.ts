30192368744655307a0086c1f89dd0f9f23802e1
unknown <swathyphpfreelancer@gmail.com>
2019-07-23 20:25:05 -0400

    implemented add domain field fn to persist fields data

--- src/app/api-generator/domain-field-generator/domain-field-generator.service.ts
+++ src/app/api-generator/domain-field-generator/domain-field-generator.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Domain } from 'src/app/model/domain';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DomainFieldGeneratorService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  public addDomainFields(domain:Domain):Observable<any>{
+    return this.httpClient.post(environment.api_url+"buildSchema",domain);
+  }
 }
