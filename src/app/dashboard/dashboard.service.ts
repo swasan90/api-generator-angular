027fb8f8841d7fd2f560e496d0a2bf8c4702e29f
unknown <swathyphpfreelancer@gmail.com>
2019-07-25 15:57:31 -0400

    completed implementing first version of dashboard page

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProjectDomain } from '../model/projectDomain';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  public listAllProjects():Observable<any>{
+    return this.httpClient.get(environment.api_url+"getProjectList");
+  }
 }
