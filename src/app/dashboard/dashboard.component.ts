1fded64ec032cde68a90c5c9126c25e09e659925
unknown <swathyphpfreelancer@gmail.com>
2019-07-25 15:57:08 -0400

    completed implementing first version of dashboard page

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,41 @@
+import { SnackbarService } from './../api-generator/snackbar.service';
 import { Component, OnInit } from '@angular/core';
+import { DashboardService } from './dashboard.service';
+import { DashboardData } from '../model/dashboard-data';
+import { ProjectDomain } from '../model/projectDomain';
+import {MatTreeNestedDataSource} from '@angular/material/tree';
+import {NestedTreeControl} from '@angular/cdk/tree';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit {  
+  treeControl = new NestedTreeControl<DashboardData>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<DashboardData>();
+  constructor(private dashboardService: DashboardService,private snackbarService:SnackbarService) { }
 
-  constructor() { }
+  hasChild = (_: number, node: DashboardData) => !!node.children && node.children.length > 0;
+ 
+  private _getChildren = (node: DashboardData) => node.children;
+
+  getAllProjects() {
+    this.dashboardService.listAllProjects().subscribe(res => {   
+      this.dataSource.data = res["projects"];        
+    },error=>{
+      console.log(error);
+      this.snackbarService.openSnackBar(error.error["message"], "Error", "custom-eror-snackbar");
+    });
+  }
+
+  getNode(node:DashboardData){
+    console.log(node);
+  }
+ 
 
   ngOnInit() {
+    this.getAllProjects();
   }
 
 }
