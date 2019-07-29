b22e5bba6f82b598404b91ae9e7bc041394bd7f8
unknown <swathyphpfreelancer@gmail.com>
2019-07-29 10:29:31 -0400

    created component class for mat card content page

---
+++ src/app/crud-processor/add-crud/mat-card-content.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { MetaData } from 'src/app/model/metaData';
+import { FormGroup } from '@angular/forms';
+import { AddCrudService } from './add-crud.service';
+
+@Component({
+  selector: 'app-card-content',
+  templateUrl: './mat-card-content.component.html',
+  styleUrls: ['./add-crud.component.css'],
+  providers: [AddCrudService]
+
+})
+export class MatCardContentComponent implements OnInit {
+ 
+  @Input() control: MetaData<any>;
+  @Input() formGroup: FormGroup;
+
+  constructor(){}
+  /**
+   * Function to return if the form is valid
+   */
+  get isValid() { 
+    return this.formGroup.controls[this.control.fieldName].valid || this.formGroup.controls[this.control.fieldName].untouched ;
+  }
+
+  ngOnInit() {
+  }
+
+  
+}
