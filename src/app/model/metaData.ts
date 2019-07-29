53057ae031502380a0acd661d2f768b8de2eb1f5
unknown <swathyphpfreelancer@gmail.com>
2019-07-29 10:31:37 -0400

    created metadata class for defining form control object structure

---
+++ src/app/model/metaData.ts
@@ -0,0 +1,21 @@
+export class MetaData<T>{
+    value: T;
+    fieldName:string;     
+    fieldType:string;
+    required?:boolean;
+    controlType?:string;
+    
+    constructor(options: {
+        value?: T,
+        fieldName?: string,         
+        required?: boolean,       
+        fieldType?: string,
+        controlType?: string
+      } = {}) {
+      this.value = options.value;
+      this.fieldName = options.fieldName || '';      
+      this.required = !!options.required;    
+      this.fieldType = options.fieldType || '';
+      this.controlType = options.controlType || '';
+    }
+}
