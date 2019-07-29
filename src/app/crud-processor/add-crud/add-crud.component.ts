9d6fb87b542eed91a3522543af142da484f01b8d
unknown <swathyphpfreelancer@gmail.com>
2019-07-29 10:49:23 -0400

    added control value accessor to register dynamic form control

--- src/app/crud-processor/add-crud/add-crud.component.ts
+++ src/app/crud-processor/add-crud/add-crud.component.ts
@@ -1,47 +1,63 @@
-import { SchemaBuilder } from './../../model/schemaBuilder';
+import { CrudProcessorService } from './../crud-processor.service';
+import { FieldType } from './../../model/fieldType';
+import { MetaData } from './../../model/metaData';
+import { FormBuilder, FormGroup, FormControl, FormArray, ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
 import { SnackbarService } from './../../api-generator/snackbar.service';
 import { ProjectDomain } from 'src/app/model/projectDomain';
-import { Component, OnInit, Input, SystemJsNgModuleLoader } from '@angular/core';
-import { CrudProcessorService } from '../crud-processor.service';
+import { Component, OnInit, Input, SystemJsNgModuleLoader, forwardRef, ViewChild, AfterViewInit } from '@angular/core'; 
 import { CdkColumnDef } from '@angular/cdk/table';
+import { Fields } from 'src/app/model/fields';
+import { AddCrudService } from './add-crud.service';
+import { TextBoxControl } from './control-type/textbox-control';
+import { AnimationKeyframesSequenceMetadata } from '@angular/animations';
 
 @Component({
   selector: 'app-add-crud',
   templateUrl: './add-crud.component.html',
-  styleUrls: ['./add-crud.component.css']
+  styleUrls: ['./add-crud.component.css'],
+  providers: [ AddCrudService ,
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => AddCrudComponent),
+      multi: true
+    } ]
 })
-export class AddCrudComponent implements OnInit {
+export class AddCrudComponent implements OnInit ,ControlValueAccessor{   
+  addFormGroup:FormGroup;
+  currentDomain: ProjectDomain;
+  formControls: MetaData<any>[] = [];
 
-  columns:string[]=[];   
-  currentDomain:ProjectDomain;
-  formObj:SchemaBuilder={};
-  ngModelValue:string;
-  constructor(private crudService:CrudProcessorService,private snackBarService:SnackbarService) { }
-
-  setColumns(displayedColumns){     
-    for(let str of displayedColumns){
-      let obj:any={};
-      obj.name = str;
-      this.columns.push(obj);
-      this.formObj[str]="";
-    }     
-  }
- 
-
-  save(record:any){
-    console.log(record);
+  constructor(private addCrudService: AddCrudService, private snackBarService: SnackbarService,private crudService:CrudProcessorService) { }
+   
+  
+  receiveEvent(form:FormGroup){
+    this.addFormGroup = form;
   }
 
+  save(){
+    console.log(this.addFormGroup.value);
+  }
+
+  public onTouched: () => void = () => { };
+
+  //Overriding control value accessor methods
+  writeValue(obj: any): void {
+    obj && this.addFormGroup.setValue(obj, { emitEvent: false });
+  }
+  registerOnChange(fn: any): void {
+    this.addFormGroup.valueChanges.subscribe(fn);
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    isDisabled ? this.addFormGroup.disable() : this.addFormGroup.enable();
+  }
+    
   ngOnInit() {
-    this.currentDomain = JSON.parse(localStorage.getItem("currentDomain"));    
-    this.crudService.getColumnNames(this.currentDomain.id).subscribe(data=>{
-      let displayedColumns:string[] = data["dataSet"];
-      this.setColumns(displayedColumns);
-      console.log(this.formObj);
-    },error=>{
-      console.log(error);
-      this.snackBarService.openSnackBar(error.error["message"], "Error", "custom-eror-snackbar");
-    });    
+    this.currentDomain = JSON.parse(localStorage.getItem("currentDomain"));      
+    this.formControls = JSON.parse(localStorage.getItem("formControls"));      
   }
 
 }
