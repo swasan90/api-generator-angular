6ad3b85d9e8d84a90f4b205adee898d4c982e35e
unknown <swathyphpfreelancer@gmail.com>
2019-07-30 14:57:42 -0400

    implemented edit fn

--- src/app/crud-processor/crud-processor.component.ts
+++ src/app/crud-processor/crud-processor.component.ts
@@ -1,12 +1,9 @@
-import { DashboardData } from './../model/dashboard-data';
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from '../dashboard/dashboard.service';
 import { ProjectDomain } from '../model/projectDomain';
 import { CrudProcessorService } from './crud-processor.service';
 import { SnackbarService } from '../api-generator/snackbar.service';
-import { Fields } from '../model/fields';
-import { MetaData } from '../model/metaData';
-import { TextBoxControl } from './add-crud/control-type/textbox-control';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-crud-processor',
@@ -17,12 +14,13 @@ export class CrudProcessorComponent implements OnInit {
   
   currentDomain:ProjectDomain;
   dataSource:[]=[];
-  constructor(private dashboardService: DashboardService,private crudService:CrudProcessorService,private snackbarService:SnackbarService) { }
+  constructor(private dashboardService: DashboardService,
+    private crudService:CrudProcessorService,private snackbarService:SnackbarService,private router:Router) { }
   displayedColumns:string[]=[];
-
+  
   loadData(){
     this.crudService.listAll(this.currentDomain.projectName,this.currentDomain.domainName).subscribe(res=>{       
-      this.dataSource = res["data"];
+      this.dataSource = res["data"];     
     },error=>{
       console.log(error);
       this.snackbarService.openSnackBar(error.error["message"], "Error", "custom-eror-snackbar");
@@ -31,12 +29,22 @@ export class CrudProcessorComponent implements OnInit {
 
   getDisplayedColumns(id:string){     
     this.crudService.getColumnNames(id).subscribe(data=>{
-      this.displayedColumns = data["dataSet"];      
+      this.displayedColumns = data["dataSet"];
+      this.displayedColumns.push("Actions");
     },error=>{
       console.log(error);
       this.snackbarService.openSnackBar(error.error["message"], "Error", "custom-eror-snackbar");
     });
   } 
+
+  addRecord(){    
+    this.router.navigate(['/api/crud_processor/add']);
+  }
+
+  edit(element:any){  
+    this.router.navigate(['/api/crud_processor/edit']);
+    this.crudService.setSelectedElement(element);
+  }
 
   ngOnInit() {
     this.currentDomain =  this.dashboardService.getSelectedDomainForCrudOps();      
