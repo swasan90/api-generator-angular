325867c99b9fa7866b705e287f5201941391449f
unknown <swathyphpfreelancer@gmail.com>
2019-08-06 16:14:23 -0400

    moved get id logic to start service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
+import { environment } from 'src/environments/environment';
+import { JwtToken } from './model/jwtToken';
+import { StartUpService } from './startup-service';
+import { AuthService } from './auth/auth.service';
+/// <reference types="crypto-js" />
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit { 
+  constructor(){} 
   title = 'api-generator';
+  ngOnInit() {
+   
+  }
 }
